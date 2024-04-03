79f154f3a9c68b40a1e5d7f49d890919cffe061c
sakshi4719 <116568621+sakshi4719@users.noreply.github.com>
2024-04-04 00:45:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function validateSignUp() {
 
+    var check = true;
+
     var usernameField = document.forms["form"]["username"];
     var usernamePrompt = document.getElementById("usernameError");
     if (usernameField.value.length < 4) {
       usernamePrompt.style.display = "block";
-      usernameField.setCustomValidity("Invalid field.");
+      document.getElementById("username-input").className = "invalid-box";
+      check = false;
     }
     else{
       usernamePrompt.style.display = "none";  
-      usernameField.setCustomValidity("");
+      document.getElementById("username-input").className = "valid-box";
     }
     
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; 
@@ -16,49 +19,59 @@ function validateSignUp() {
     var mailPrompt = document.getElementById("mailError");
     if ( !(mailField.value.match(validRegex)) ) {
       mailPrompt.style.display = "block";
-      mailField.setCustomValidity("Invalid field.");
+      document.getElementById("email-input").className = "invalid-box";
+      check = false;
     }
     else{
       mailPrompt.style.display = "none";  
-      mailField.setCustomValidity("");
+      document.getElementById("email-input").className = "valid-box";
     }
 
     var pwField = document.forms["form"]["password"];
     var pwPrompt = document.getElementById("passwordError");
     if (pwField.value.length < 8){
       pwPrompt.style.display = "block";
-      pwField.setCustomValidity("Invalid field.");
+      document.getElementById("pw-input").className = "invalid-box";
+      check = false;
     }
     else{
       pwPrompt.style.display = "none";  
-      pwField.setCustomValidity("");
+      document.getElementById("pw-input").className = "valid-box";
     }
     
     var cpwField = document.forms["form"]["cpassword"];
     var cpwPrompt = document.getElementById("confirmationError");
     if ( !(cpwField.value === pwField.value) ){
       cpwPrompt.style.display = "block";
-      cpwField.setCustomValidity("Invalid field.");
+      document.getElementById("cpw-input").className = "invalid-box";
+      check = false;
     }
     else{
       cpwPrompt.style.display = "none";  
-      cpwField.setCustomValidity("");
+      document.getElementById("cpw-input").className = "valid-box";
     }
+
+    return check;
 }
   
 function validateLogin(){
+
+    var check = true;
 
     var usernameField = document.forms["form"]["username"];
     var pwField = document.forms["form"]["password"];
     var prompt = document.getElementById("error");
     if (usernameField.value.length < 4 || pwField.value.length < 8) {
       prompt.style.display = "block";
-      usernameField.setCustomValidity("Invalid field.");
-      pwField.setCustomValidity("Invalid field.");
+      document.getElementById("username-input").className = "invalid-box";
+      document.getElementById("pw-input").className = "invalid-box";
+      check = false;
     }
     else{
       prompt.style.display = "none";  
-      usernameField.setCustomValidity("");
-      pwField.setCustomValidity("");
+      document.getElementById("username-input").className = "valid-box";
+      document.getElementById("pw-input").className = "valid-box";
     }
+
+    return check;
 }
